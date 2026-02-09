84a844124b998c3c51b645628e5a122f1d19a6a7
Tiago <tiago.amancio@escola.pr.gov.br>
2026-02-09 14:11:22 +0000

    aula 1

---
+++ calculadora.py
@@ -0,0 +1,8 @@
+num1 = int(input("digite o primeiro número"))
+num2 = int(input("digite o segundo número"))
+
+soma = num1+num2
+
+sub = num1-num2 
+
+print("A soma é : ",soma)
---
+++ ola.py
@@ -0,0 +1,5 @@
+#nome = input("digite seu nome: ")
+#print("olá, ", nome ,"seja bem vindo")
+
+sobrenome = input("digite seu sobrenome: ")
+print("olá, ", sobrenome,"seja bem vindo")
