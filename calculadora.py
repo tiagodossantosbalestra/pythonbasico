8fff9fc10ff8f55a3a3127a3d7a866742ec22070
Tiago <tiago.amancio@escola.pr.gov.br>
2026-02-10 11:59:15 +0000

    aula2

--- calculadora.py
+++ calculadora.py
@@ -2,7 +2,11 @@ num1 = int(input("digite o primeiro número"))
 num2 = int(input("digite o segundo número"))
 
 soma = num1+num2
-
 sub = num1-num2 
+div = num1/num2
+multi = num1*num2
 
-print("A soma é : ",soma)
+print("A soma é: ",soma)
+print("A subtração é: ",sub)
+print("A divisão é: ",div)
+print("a multiplicação é: ",multi)
