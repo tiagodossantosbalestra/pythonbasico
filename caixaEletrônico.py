217c43c18707dfdc9ca20e9281d2db2c1c47d22e
Tiago <tiago.amancio@escola.pr.gov.br>
2026-02-10 13:55:28 +0000

    quinta alteração

---
+++ caixaEletrônico.py
@@ -0,0 +1,14 @@
+while True:
+    try:
+        valor = float(input("Digite o valor do saque: R$ "))
+
+        if valor <= 0:
+            print("O valor deve ser maior que zero.")
+        elif valor > 1000:
+            print("Saque não permitido. O limite é R$ 1000.")
+        else:
+            print(f"Saque de R$ {valor:.2f} realizado com sucesso!")
+            break
+
+    except ValueError:
+        print("Entrada inválida. Digite apenas números.")
