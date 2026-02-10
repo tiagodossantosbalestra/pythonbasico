4fbbfe1f5e22d9cfe16b64fbb0299c7db1b9e31c
Tiago <tiago.amancio@escola.pr.gov.br>
2026-02-10 12:40:15 +0000

    aula3

---
+++ menu.py
@@ -0,0 +1,14 @@
+while True:
+    print("1- cadastrar")
+    print("2- consultar")
+    print("3- sair")
+
+    opcao = input("escolha uma opção: ")
+
+    if opcao in ["1","2","3"]:
+        break
+    else:
+        print("opção inválida! - escolha novamente")
+
+print("você escolheu: ",opcao)
+    
---
+++ textoVasio.py
@@ -0,0 +1,9 @@
+while True:
+    nome = input("digite seu nome: ").strip()
+
+    if nome =="":
+        print("O nome não pode ser vazio. ")
+    else:
+        break
+
+    print("nome", nome)
