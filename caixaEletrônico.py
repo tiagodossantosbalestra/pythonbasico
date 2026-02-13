480ecd82057d56116eb3012afa993070cd2ee630
Tiago <tiago.amancio@escola.pr.gov.br>
2026-02-13 11:11:18 +0000

    aula4

--- caixaEletrônico.py
+++ caixaEletrônico.py
@@ -1,14 +1,15 @@
-while True:
+while true:
     try:
-        valor = float(input("Digite o valor do saque: R$ "))
+        valor = float(input("Digite o valor do saque: r$ "))
 
-        if valor <= 0:
+        if valor <=0:
             print("O valor deve ser maior que zero.")
         elif valor > 1000:
-            print("Saque não permitido. O limite é R$ 1000.")
+             print("Saque não permitido. O limite é R$ 1000.")
         else:
             print(f"Saque de R$ {valor:.2f} realizado com sucesso!")
-            break
+           break
 
     except ValueError:
-        print("Entrada inválida. Digite apenas números.")
+       print("Entrada inválida. Digite apenas números.")
+
