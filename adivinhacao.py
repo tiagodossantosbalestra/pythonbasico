04855ec0d27f35dc4830e0a77de47faea1c1f4ec
Tiago <tiago.amancio@escola.pr.gov.br>
2026-02-24 12:55:37 +0000

    14

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,66 +1,37 @@
 import random
-
 print('************')
-print('**** Jogo de Adivinhação *****')
+print('****jogo adivinhação*****')
 print('*************')
 
-# Escolha do nível de dificuldade
-print("Escolha um nível de dificuldade:")
-print("1. Fácil")
-print("2. Médio")
-print("3. Difícil")
+numero_secreto = random.randrange(1,31)
+total_tentativas=3
 
-nivel = input("Digite o número do nível (1, 2 ou 3): ")
-
-if nivel == "1":
-    total_tentativas = 5
-    numero_secreto = random.randrange(1, 21)  # Faixa de 1 a 20
-elif nivel == "2":
-    total_tentativas = 3
-    numero_secreto = random.randrange(1, 31)  # Faixa de 1 a 30
-elif nivel == "3":
-    total_tentativas = 1
-    numero_secreto = random.randrange(1, 51)  # Faixa de 1 a 50
-else:
-    print("Opção inválida. O nível será definido como 'Médio'.")
-    total_tentativas = 3
-    numero_secreto = random.randrange(1, 31)
-
-print(f"Nível escolhido: {nivel}")
-print(f"O número secreto foi definido. Você tem {total_tentativas} tentativas.")
-
-# Iniciar o loop de tentativas
 for rodada in range(1, total_tentativas + 1):
-    print(f"Tentativa {rodada} de {total_tentativas}")
+    print("tentativa{} de {}".format(rodada,total_tentativas))
 
-    # Obter o chute do jogador com verificação de entrada
-    while True:
-        try:
-            chute_str = input(f"Digite um número entre 1 e {numero_secreto}: ")
-            chute = int(chute_str)
-            if 1 <= chute <= numero_secreto:
-                break
-            else:
-                print(f"Você deve digitar um número entre 1 e {numero_secreto}.")
-        except ValueError:
-            print("Entrada inválida! Por favor, digite um número inteiro.")
+while (rodada <= total_tentativas):
 
-    print(f"Seu número é: {chute}")
+    chute_str = input("digite o seu número: ")
+    print("seu número é: ", chute_str)
+
+    chute = int(chute_str)
+
+    if(chute< 1 or chute>30):
+        print("você deve digitar um número entre 1 e 30! ")
+        continue
 
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
     menor = chute < numero_secreto
 
-    if acertou:
-        print("Você acertou!!")
-        break  # Sai do loop, jogo terminado
+    if(acertou):
+        print("você Acertou!! ")
+        break
     else:
-        if maior:
-            print("O seu chute foi maior que o número secreto.")
-        elif menor:
-            print("O seu chute foi menor que o número secreto.")
+        if(maior):
+            print("o seuchute foi maior que o numero secreto")
+        elif(menor):
+            print("o seu chute foi menor que o numero secreto")
+    rodada = rodada +1
 
-# Fim do jogo
-if not acertou:
-    print(f"Você não acertou! O número secreto era {numero_secreto}.")
-print("Fim de jogo!")
+print("fim de jogo!")
